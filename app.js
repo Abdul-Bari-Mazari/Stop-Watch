5695c83b717b06d0f0309e16977ce4e45a1c3f57
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-05-09 18:39:10 +0500

    Completed!
    -Added Reset button functionality for timer and stopwatch
    -Fixed some bugs

--- app.js
+++ app.js
@@ -59,10 +59,18 @@ const minutesHTML = document.getElementById("minutes"),
 
 // Start timer function
 
-function startTimer() {
-  let seconds = 0,
-    minutes = 5;
+let interval;
+let play = false;
 
+// function startTimer() {
+
+// }
+
+// Start timer button event
+
+let seconds = 0,
+  minutes = 5;
+function startInterval() {
   if (seconds <= 0) {
     if (minutes <= 0) {
       clearInterval(interval);
@@ -78,26 +86,29 @@ function startTimer() {
   secondsHTML.innerHTML = seconds;
 }
 
-let interval = setInterval(startTimer, 1000);
-
-// Start timer button event
-
 function startTimerBtn() {
-  startTimer();
-  if ((startTimerButton.innerHTML = "START")) {
-    startTimerButton.innerHTML = "STOP";
-  } else if (startTimerButton.innerHTML === "STOP") {
-    startTimerButton.innerHTML = "START";
+  //   startTimer();
+  if (play) {
     clearInterval(interval);
+    play = false;
+    event.target.innerText = "START";
+  } else {
+    interval = setInterval(startInterval, 1000);
+    event.target.innerText = "STOP";
+    play = true;
   }
 }
 
 // Reset timer button event
 
 function resetTimer() {
+  play = false;
+  startTimerButton.innerText = "START";
+  clearInterval(interval);
+  minutes = 5;
+  seconds = 0;
   minutesHTML.innerHTML = "5";
   secondsHTML.innerHTML = "00";
-  clearInterval(interval);
 }
 
 // Stopwatch second and milisecond elements
@@ -108,34 +119,50 @@ const watchSeconds = document.getElementById("watchSeconds"),
 
 // Start stopwatch function
 
-let interval1;
+let interval1,
+  timer = 0,
+  stopWatchSeconds = 0,
+  watchMinutes = 0,
+  playWatch = false;
+
+function startWatchInterval() {
+  timer++;
+  if (timer === 100) {
+    stopWatchSeconds++;
+    timer = 0;
+    if (stopWatchSeconds === 60) {
+      stopWatchSeconds = 0;
+      stopWatchMinutes.style.display = "flex";
+      watchMinutes++;
+    }
+  }
+  stopWatchMinutes.innerHTML = `<h1>${watchMinutes}</h1>
+    <h5>m</h5>`;
+  watchSeconds.innerHTML = stopWatchSeconds;
+  miliseconds.innerHTML = timer;
+}
 
 function startWatch() {
-  let timer = 0,
-    stopWatchSeconds = 0,
-    watchMinutes = 0,
-    interval1 = setInterval(function () {
-      timer++;
-      if (timer === 100) {
-        stopWatchSeconds++;
-        timer = 0;
-        if (stopWatchSeconds === 60) {
-          stopWatchSeconds = 0;
-          stopWatchMinutes.style.display = "flex";
-          watchMinutes++;
-        }
-      }
-      stopWatchMinutes.innerHTML = `<h1>${watchMinutes}</h1>
-    <h5>m</h5>`;
-      watchSeconds.innerHTML = stopWatchSeconds;
-      miliseconds.innerHTML = timer;
-    }, 10);
+  if (play) {
+    clearInterval(interval1);
+    event.target.innerText = "START";
+    play = false;
+  } else {
+    interval1 = setInterval(startWatchInterval, 10);
+    event.target.innerText = "STOP";
+    play = true;
+  }
 }
 
 // Reset stopwatch function
 
 function resetWatch() {
-  watchSeconds.innerHTML = "0";
-  miliseconds.innerHTML = "00";
   clearInterval(interval1);
+  startWatchButton.innerText = "START";
+  play = false;
+  (timer = 0),
+    (stopWatchSeconds = 0),
+    (watchMinutes = 0),
+    (watchSeconds.innerHTML = "0");
+  miliseconds.innerHTML = "00";
 }
